320d64b6c686e22ed7e46ab586071229e24dd34d
AgronC <agron.developer@gmail.com>
2019-03-26 00:56:41 +1100

    Added IHS (indoor Health Score) feature

--- js/model.js
+++ js/model.js
@@ -2,62 +2,109 @@ function fire() {
   // Get a reference to the database service
   var database = firebase.database();
 
-  //GET last child and return val for temperature;
+  //Store in a variable the last batch of sensor data added to Firebase
   var lastChild_Rec = firebase
     .database()
     .ref("avr-iot/data/01234245A3E58A0BFE/")
     .limitToLast(1);
 
-  //Get Last TEMP
+  //Variables for computing total health score
+  var tempTotal = 0;
+  var airTotal = 0;
+  var lightTotal = 0;
+  //Get Temperature of the last item added in firebase
   lastChild_Rec.on("child_added", function(lastdatasnap) {
     var tempVal = lastdatasnap.child("Temp").val();
 
-    if (tempVal > 40) {
+    if (tempVal > 26) {
+      //red for upperboud/hot temperature
       document.getElementById("lastTemp").innerText = tempVal;
       document.getElementById("statsId_temp").style.borderColor = "#DC143C";
-    } else if (tempVal < 40 && tempVal > 30) {
+      tempTotal = 20;
+    } else if (tempVal < 27 && tempVal > 24) {
+      //orange for non-optimal orange
       document.getElementById("lastTemp").innerText = tempVal;
       document.getElementById("statsId_temp").style.borderColor = "#FF6347";
       document.getElementById("tempSmsAlert").innerHTML =
         "<div class='alert alert-info alert-dismissible'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>SMS Alert Sent</strong> Temperature has reached the recommended threshold.</div>";
+      tempTotal = 75;
+    } else if (tempVal < 21 && tempVal > 18) {
+      //orange for non-optimal orange
+      document.getElementById("lastTemp").innerText = tempVal;
+      document.getElementById("statsId_temp").style.borderColor = "#FF6347";
+      document.getElementById("tempSmsAlert").innerHTML =
+        "<div class='alert alert-info alert-dismissible'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>SMS Alert Sent</strong> Temperature has reached the recommended threshold.</div>";
+      tempTotal = 75;
+    } else if (tempVal < 20) {
+      //change to blue when cold (lowerbound)
+      document.getElementById("lastTemp").innerText = tempVal;
+      document.getElementById("statsId_temp").style.borderColor = "#4787ff";
+      document.getElementById("tempSmsAlert").innerHTML =
+        "<div class='alert alert-info alert-dismissible'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>SMS Alert Sent</strong> Temperature has reached the recommended threshold.</div>";
+      tempTotal = 20;
     } else {
       document.getElementById("lastTemp").innerText = tempVal;
       document.getElementById("statsId_temp").style.borderColor = "#01cd74";
+      tempTotal = 90;
     }
   });
 
-  //Get Last AirQuality
+  //Get Air Quality value of the last item added in firebase
   lastChild_Rec.on("child_added", function(lastdatasnap) {
     var AirQualityVal = lastdatasnap.child("AirQuality").val();
 
-    if (AirQualityVal > 40) {
+    if (AirQualityVal > 999) {
+      //very poor air quality range (needs calibration based on sensor)
       document.getElementById("lastAirQuality").innerText = AirQualityVal;
       document.getElementById("statsId_airquality").style.borderColor =
         "#DC143C";
-    } else if (AirQualityVal < 40 && AirQualityVal > 30) {
+      airTotal = 20;
+    } else if (AirQualityVal < 1000 && AirQualityVal > 350) {
+      //poor air quality range
       document.getElementById("lastAirQuality").innerText = AirQualityVal;
       document.getElementById("statsId_airquality").style.borderColor =
         "#FF6347";
+      airTotal = 75;
     } else {
+      //Fresh air quality range
       document.getElementById("lastAirQuality").innerText = AirQualityVal;
       document.getElementById("statsId_airquality").style.borderColor =
         "#01cd74";
+      airTotal = 90;
     }
   });
 
-  //Get Last Light
+  //Get Light sensor value of the last item added in firebase
+  //http://www.resourcesmart.vic.gov.au/documents/lux_meter.pdf
   lastChild_Rec.on("child_added", function(lastdatasnap) {
-    var tempVal = lastdatasnap.child("Light").val();
+    var lightVal = lastdatasnap.child("Light").val();
 
-    if (tempVal > 40) {
-      document.getElementById("lastLight").innerText = tempVal;
+    if (lightVal > 320) {
+      //upperbound negative range - too much light
+      document.getElementById("lastLight").innerText = lightVal;
       document.getElementById("statsId_light").style.borderColor = "#DC143C";
-    } else if (tempVal < 40 && tempVal > 30) {
-      document.getElementById("lastLight").innerText = tempVal;
+      lightTotal = 20;
+    } else if (lightVal < 80) {
+      //lowerbound negative range - not enough light
+      document.getElementById("lastLight").innerText = lightVal;
+      document.getElementById("statsId_light").style.borderColor = "#DC143C";
+      lightTotal = 20;
+    } else if (lightVal < 160) {
+      //undersired range for office/desk areas
+      document.getElementById("lastLight").innerText = lightVal;
       document.getElementById("statsId_light").style.borderColor = "#FF6347";
+      lightTotal = 75;
     } else {
-      document.getElementById("lastLight").innerText = tempVal;
+      //optimal range between 160 - 320
+      document.getElementById("lastLight").innerText = lightVal;
       document.getElementById("statsId_light").style.borderColor = "#01cd74";
+      lightTotal = 90;
     }
+    function computeTotalScore() {
+      //compute total score /100.
+      var healthScore = Math.round((tempTotal + airTotal + lightTotal) / 3);
+      document.getElementById("HealthScoreValue").innerText = healthScore;
+    }
+    computeTotalScore();
   });
 }
